74c597409bf6cbe8870abd8da51ec4fe9a45356c
ririndeismillia <ririndeismillia@gmail.com>
2026-03-02 22:40:31 +0700

    commit4

---
+++ fungsiparameter.cpp
@@ -0,0 +1,11 @@
+//library
+#include <iostream>
+using namespace std;
+//deklarasi variabel global
+int panjang, lebar;
+void input(){
+    cout << "Masukkan Panjang : ";
+    cin >> panjang;
+    cout << "Masukkan Lebar : ";
+    cin >> lebar;
+}
